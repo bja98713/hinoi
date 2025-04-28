4ea60b05872ac2b0725012996b7239963aba7117
bronstein <bronstein@MacBookPro.fritz.box>
2025-04-28 09:53:16 -1000

    maj3

--- comptabilite/admin.py
+++ comptabilite/admin.py
@@ -3,21 +3,25 @@ from .models import Code, Medecin
 
 @admin.register(Code)
 class CodeAdmin(admin.ModelAdmin):
+    # Affichage en liste
     list_display = (
-        'code_acte',
-        'total_acte',
-        'tiers_payant',
-        'total_paye',
-        'medecin',
-        'parcours_soin',
-        'longue_maladie',
-        'code_reel',
-        'variable_1',
-        'code_acte_normal',
-        'variable_2',
-        'modificateur'
+        'code_acte', 'total_acte', 'tiers_payant', 'total_paye',
+        'medecin', 'parcours_soin', 'longue_maladie',
+        'code_reel', 'variable_1', 'code_acte_normal',
+        'variable_2', 'modificateur'
     )
     search_fields = ('code_acte', 'code_acte_normal', 'code_reel')
+
+    # On récupère tous les champs non M2M ni O2M, triés alphabétiquement
+    field_names = sorted(
+        f.name for f in Code._meta.get_fields()
+        if not (f.many_to_many or f.one_to_many)
+    )
+
+    fields = field_names
+    list_display = field_names
+    ordering = field_names
+
 
 @admin.register(Medecin)
 class MedecinAdmin(admin.ModelAdmin):
